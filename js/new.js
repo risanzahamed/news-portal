c9fb2e801818ded395d4b7ce2f0e001ec5f870cc
Risan Ahamed <risanzahamed76@gmail.com>
2022-09-04 01:16:20 +0600

    commited

--- js/new.js
+++ js/new.js
@@ -1,21 +1,56 @@
-const newsShow = () => {
+const newsCategoryShow = () => {
     fetch("https://openapi.programming-hero.com/api/news/categories")
         .then(res => res.json())
         .then(data => displayCategories(data.data.news_category))
 }
 
-const displayCategories = (product) => {
+const displayCategories = (newsportal) => {
     const categoriesContainer = document.getElementById("categories")
-    product.forEach(news => {
+    newsportal.forEach(news => {
         console.log(news)
         const categoryDiv = document.createElement("div")
-
         categoryDiv.innerHTML = `
-        <h1 class=" p-5 ">${news.category_name}</h1>
+        <button onclick="newDetail()"  class=" p-3 ">${news.category_name}</button>
     `
         categoriesContainer.appendChild(categoryDiv)
+        
     });
 
 }
 
-newsShow()
+
+newsCategoryShow()
+
+const newDetail = (_id) => {
+    const url = (`https://openapi.programming-hero.com/api/news/category/08`)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNewsCard(data.data))
+}
+
+const displayNewsCard = (newsDisplay) => {
+    const newsdetailsContainer = document.getElementById("news-card")
+    newsDisplay.forEach(newsDetails => {
+        console.log(newsDetails)
+        const newsportalDiv = document.createElement("div")
+        newsportalDiv.innerHTML = `
+        <div class="card">
+            <img src="${newsDetails.image_url} ? ${newsDetails.image_url} : No Image " class="card-img-top" alt="...">
+            <div class="card-body">
+            <h1 class="card-title sm:text-1xl">News Title : ${newsDetails.title.slice(0,55)} </h1>
+            <p class="card-text sm:text-1xl">${newsDetails.details.slice(0,250)}</p>
+            
+            <div class="d-flex justify-content-between align-middle mt-2">
+            <h1 class="card-title p-1 sm:text-sm">Author: ${newsDetails.author.name}, </h1> <p class="p-1 ps-5 mt-1">View: ${newsDetails.total_view}k</p>
+            <p class="ps- mt-2"">Rating: ${newsDetails.rating.number}</p></div>
+            
+            </div>
+        </div>          
+        `
+        newsdetailsContainer.appendChild(newsportalDiv)
+    });
+}
+
+
+
+newDetail()
