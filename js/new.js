59d8429573eae13cedadb2379b29601cf75dff20
Risan Ahamed <risanzahamed76@gmail.com>
2022-09-04 06:38:36 +0600

    commited

--- js/new.js
+++ js/new.js
@@ -10,10 +10,10 @@ const displayCategories = (newsportal) => {
         console.log(news)
         const categoryDiv = document.createElement("div")
         categoryDiv.innerHTML = `
-        <button onclick="newDetail()"  class=" p-3 ">${news.category_name}</button>
+        <button onclick = "newDetail()" class=" p-3 ">${news.category_name}</button>
     `
         categoriesContainer.appendChild(categoryDiv)
-        
+
     });
 
 }
@@ -21,7 +21,7 @@ const displayCategories = (newsportal) => {
 
 newsCategoryShow()
 
-const newDetail = (_id) => {
+const newDetail = () => {
     const url = (`https://openapi.programming-hero.com/api/news/category/08`)
     fetch(url)
         .then(res => res.json())
@@ -30,6 +30,7 @@ const newDetail = (_id) => {
 
 const displayNewsCard = (newsDisplay) => {
     const newsdetailsContainer = document.getElementById("news-card")
+    newsdetailsContainer.textContent = ""
     newsDisplay.forEach(newsDetails => {
         console.log(newsDetails)
         const newsportalDiv = document.createElement("div")
@@ -37,15 +38,19 @@ const displayNewsCard = (newsDisplay) => {
         <div class="card">
             <img src="${newsDetails.image_url} ? ${newsDetails.image_url} : No Image " class="card-img-top" alt="...">
             <div class="card-body">
-            <h1 class="card-title sm:text-1xl">News Title : ${newsDetails.title.slice(0,55)} </h1>
-            <p class="card-text sm:text-1xl">${newsDetails.details.slice(0,250)}</p>
+            <h1 class="card-title sm:text-1">News Title : ${newsDetails.title.length > 40 ? newsDetails.title.slice(0, 40) + "..." : newsDetails.title} </h1>
+            <p class="card-text">${newsDetails.details.length > 110 ? newsDetails.details.slice(0, 110) + '...' : newsDetails.details}</p>
             
             <div class="d-flex justify-content-between align-middle mt-2">
-            <h1 class="card-title p-1 sm:text-sm">Author: ${newsDetails.author.name}, </h1> <p class="p-1 ps-5 mt-1">View: ${newsDetails.total_view}k</p>
+            <img src="${newsDetails.author.img} ? ${newsDetails.author.img} : No Image " class="card-img-top w-10 h-10 rounded-90" alt="...">
+            <h1 class="card-title ps-1 pt-1  text-xs">Author: ${newsDetails.author.name ? newsDetails.author.name : "No Name"}
+            </h1> <p class="p-1 ps-5 mt-1">View: ${newsDetails.total_view}k</p>
             <p class="ps- mt-2"">Rating: ${newsDetails.rating.number}</p></div>
-            
             </div>
-        </div>          
+            </div>
+            
+        </div>  
+                
         `
         newsdetailsContainer.appendChild(newsportalDiv)
     });
@@ -53,4 +58,4 @@ const displayNewsCard = (newsDisplay) => {
 
 
 
-newDetail()
+// newDetail()
